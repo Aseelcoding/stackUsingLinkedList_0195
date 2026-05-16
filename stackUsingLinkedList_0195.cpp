ea8f26bf8c97f7704c4eb07a8da272e05588d6b5
Aseel <asilalhashmi@email.com>
2026-05-16 15:59:46 +0700

    Update top pointer during pop

--- stackUsingLinkedList_0195.cpp
+++ stackUsingLinkedList_0195.cpp
@@ -39,13 +39,13 @@ public:
     }
     void pop()
     {
-       
         if (isEmpty())
         {
             cout << "Stack is empty. Cannot pop." << endl;
         }
 
         Node* temp = top;
+        top = top->next;
     }
 };
 int main()
